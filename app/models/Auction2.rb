fa1c784b2a09a74da3a81fdd176a50732ce1e365
Bruce Hubbard <brucehubbard@gmail.com>
2012-05-21 16:34:46 -0400

    Removing redundant model

--- app/models/Auction2.rb
+++
@@ -1,8 +0,0 @@
-class Auction
-  attr_accessor :date, :plaintiff, :defendant, :address, :caseNumber, :appraised, :startingBid
-  
-  def to_s
-    "Auction<date=#{self.date}, plaintiff=#{self.plaintiff}, address=#{self.address}, " +
-    "appraised:#{self.appraised}, startingBid:#{self.startingBid}"
-  end
-end
